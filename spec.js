a67522f2e478d49ccb132bf8abcd504a1819d3a5
Manjusha <manjushagp96@gmail.com>
2021-10-21 11:06:09 +0530

    Add files via upload

--- spec.js
+++ spec.js
@@ -1,8 +1,11 @@
-describe('Protractor Demo google', () => {
-	it('Should navigate to Demo Page', () => {
-		browser.get('https://www.google.com/');
-		browser.getCurrentUrl().then((url) => {
-			expect(url).toBe('https://www.google.com/');
+describe('Login Demo',()=> {
+	it('Protractor Login', () => { 
+		browser.waitForAngularEnabled(false); 
+			browser.get('http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx') 
+			.then(() => element(by.id('ctl00_MainContent_username')).sendKeys('Tester'))  // then keyword is used to chain
+			.then(() => element(by.id('ctl00_MainContent_password')).sendKeys('test')) 
+            .then(() => element(by.name('ctl00$MainContent$login_button')).click())
+			browser.getCurrentUrl().then((url) => {
+				expect(url).toBe('http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/default.aspx');})
 		});
 	});
-});
